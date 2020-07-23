0a350637b7a803cba5bf88c6d78cf758a58ee2b8
Denys Kozii <denys.kozii@gmail.com>
2020-07-23 20:31:54 +0300

    added methods to SprintService and MarathonService.

--- src/main/java/com/softserve/sprint13/service/MarathonServiceImpl.java
+++ src/main/java/com/softserve/sprint13/service/MarathonServiceImpl.java
@@ -1,5 +1,28 @@
 package com.softserve.sprint13.service;
 
+import com.softserve.sprint13.entity.Marathon;
+
+import java.util.List;
+
 public class MarathonServiceImpl implements MarathonService{
 
+    @Override
+    public List<Marathon> getAll() {
+        return null;
+    }
+
+    @Override
+    public Marathon getMarathonById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Marathon createOrUpdate(Marathon marathon) {
+        return null;
+    }
+
+    @Override
+    public void deleteMarathonById(Long id) {
+
+    }
 }
--- src/main/java/com/softserve/sprint13/service/SprintService.java
+++ src/main/java/com/softserve/sprint13/service/SprintService.java
@@ -1,4 +1,11 @@
 package com.softserve.sprint13.service;
 
+import com.softserve.sprint13.entity.Marathon;
+import com.softserve.sprint13.entity.Sprint;
+
 public interface SprintService {
+    public Sprint getSprintByMarathonId(Sprint sprint, Marathon marathon);
+    public boolean addSprintToMarathon(Sprint sprint, Marathon marathon);
+    public boolean updateSprint(Sprint sprint);
+    public Sprint getSprintById(Long id);
 }
