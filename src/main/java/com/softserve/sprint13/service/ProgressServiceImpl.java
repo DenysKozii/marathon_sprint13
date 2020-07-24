c32b53ef811159fbac27b0482557d89a764f6e94
Denys Kozii <denys.kozii@gmail.com>
2020-07-24 09:42:05 +0300

    added getProgressById and repository field

--- src/main/java/com/softserve/sprint13/service/ProgressServiceImpl.java
+++ src/main/java/com/softserve/sprint13/service/ProgressServiceImpl.java
@@ -1,15 +1,27 @@
 package com.softserve.sprint13.service;
 
 import com.softserve.sprint13.entity.Progress;
+import com.softserve.sprint13.entity.Sprint;
 import com.softserve.sprint13.entity.Task;
+import com.softserve.sprint13.repository.ProgressRepository;
 import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.Optional;
 
 public class ProgressServiceImpl implements ProgressService {
+
+    @Autowired
+    ProgressRepository progressRepository;
+
     @Override
     public Progress getProgressById(Long id) {
-        return null;
+        Optional<Progress> progress = progressRepository.findById(id);
+        if (progress.isPresent())
+            return progress.get();
+        else throw new EntityNotFoundException("No progress for given id");
     }
 
     @Override
