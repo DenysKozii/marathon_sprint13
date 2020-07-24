0b4e69bd9e8f019085c95f25853a5acfac2f13bf
Denys Kozii <denys.kozii@gmail.com>
2020-07-24 09:39:48 +0300

    fixed bag in ProgressRepository

--- src/main/java/com/softserve/sprint13/repository/ProgressRepository.java
+++ src/main/java/com/softserve/sprint13/repository/ProgressRepository.java
@@ -1,9 +1,10 @@
 package com.softserve.sprint13.repository;
 
 import com.softserve.sprint13.entity.Marathon;
+import com.softserve.sprint13.entity.Progress;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProgressRepository extends JpaRepository<Process,Long> {
+public interface ProgressRepository extends JpaRepository<Progress,Long> {
 }
