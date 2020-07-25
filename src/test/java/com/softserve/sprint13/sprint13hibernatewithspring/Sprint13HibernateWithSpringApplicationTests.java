d1565ceceb6078a182598fd652c20dd995fbbc6d
Denys Kozii <denys.kozii@gmail.com>
2020-07-25 09:39:32 +0300

    commit before merge

--- src/test/java/com/softserve/sprint13/sprint13hibernatewithspring/Sprint13HibernateWithSpringApplicationTests.java
+++ src/test/java/com/softserve/sprint13/sprint13hibernatewithspring/Sprint13HibernateWithSpringApplicationTests.java
@@ -1,13 +1,26 @@
 package com.softserve.sprint13.sprint13hibernatewithspring;
 
+import com.softserve.sprint13.entity.Progress;
+import com.softserve.sprint13.service.ProgressService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 @SpringBootTest
 class Sprint13HibernateWithSpringApplicationTests {
 
+
     @Test
     void contextLoads() {
+
     }
+
 
 }
