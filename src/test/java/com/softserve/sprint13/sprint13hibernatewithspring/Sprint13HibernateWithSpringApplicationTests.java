291f657715c585b5904ccb3f075b528c6b438b62
Vyacheslav Chernykh <chernihvyacheslav@gmail.com>
2020-07-25 17:40:32 +0300

    Fixed tests for UserService

--- src/test/java/com/softserve/sprint13/sprint13hibernatewithspring/Sprint13HibernateWithSpringApplicationTests.java
+++ src/test/java/com/softserve/sprint13/sprint13hibernatewithspring/Sprint13HibernateWithSpringApplicationTests.java
@@ -1,11 +1,14 @@
 package com.softserve.sprint13.sprint13hibernatewithspring;
 
-import com.softserve.sprint13.entity.*;
+import com.softserve.sprint13.entity.Marathon;
+import com.softserve.sprint13.entity.Sprint;
+import com.softserve.sprint13.entity.Task;
+import com.softserve.sprint13.entity.User;
 import com.softserve.sprint13.service.*;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
 
 import java.sql.Date;
 import java.time.LocalDate;
@@ -14,6 +17,7 @@ import java.util.Comparator;
 import java.util.List;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class Sprint13HibernateWithSpringApplicationTests {
 
     @Autowired
@@ -36,7 +40,6 @@ class Sprint13HibernateWithSpringApplicationTests {
         this.progressService = progressService;
         this.sprintService = sprintService;
         this.marathonService = marathonService;
-        fillDataBase();
     }
 
     @Test
@@ -48,8 +51,8 @@ class Sprint13HibernateWithSpringApplicationTests {
         Assertions.assertNotNull(marathonService);
     }
 
+    @BeforeTestClass
     private void fillDataBase() {
-        clearTables();
         Marathon marathon = new Marathon();
         marathon.setTitle("Marathon1");
         marathonService.createOrUpdateMarathon(marathon);
@@ -97,56 +100,8 @@ class Sprint13HibernateWithSpringApplicationTests {
         }
     }
 
-    private void clearTables() {
-
-    }
-
     @Test
-    public void checkUpdateUsers() {
-        User mentor4 = userService.getUserById(1L);
-        User mentor7 = userService.getUserById(3L);
-        User trainee1 = userService.getUserById(1L);
-        List<User> actual = new ArrayList<>();
-        actual.add(mentor4);
-        actual.add(mentor7);
-        actual.add(trainee1);
-        mentor4.setFirstName("ChangedMentor4");
-        mentor7.setFirstName("ChangedMentor7");
-        trainee1.setFirstName("ChangedTrainee1");
-        userService.createOrUpdateUser(mentor4);
-        userService.createOrUpdateUser(mentor7);
-        userService.createOrUpdateUser(trainee1);
-
-        List<User> expected = new ArrayList<>();
-
-        User user1 = new User();
-        user1.setEmail("mentoruser3@dh.com");
-        user1.setFirstName("ChangedMentor4");
-        user1.setLastName("MentorSurname3");
-        user1.setPassword("qwertyqwerty3");
-        user1.setRole(User.Role.MENTOR);
-        expected.add(user1);
-
-        User user2 = new User();
-        user2.setEmail("mentoruser6@dh.com");
-        user2.setFirstName("ChangedMentor7");
-        user2.setLastName("MentorSurname6");
-        user2.setPassword("qwertyqwerty6");
-        user2.setRole(User.Role.MENTOR);
-        expected.add(user2);
-
-        User user3 = new User();
-        user3.setEmail("traineeUser0@dh.com");
-        user3.setFirstName("ChangedTrainee1");
-        user3.setLastName("TraineeSurname0");
-        user3.setPassword("qwertyqwerty0");
-        user3.setRole(User.Role.TRAINEE);
-        expected.add(user3);
-
-        Assertions.assertEquals(expected, actual, "checkUpdateUsers()");
-    }
-
-    @Test
+    @Order(1)
     public void checkGetAllUsersByRole() {
         List<User> actualMentors = userService.getAllByRole(User.Role.MENTOR.toString());
         List<User> actualTrainees = userService.getAllByRole(User.Role.TRAINEE.toString());
@@ -157,26 +112,71 @@ class Sprint13HibernateWithSpringApplicationTests {
         List<User> expectedMentors = new ArrayList<>();
         List<User> expectedTrainees = new ArrayList<>();
 
-        for (int i = 0; i < 10; i++) {
-            User user = new User();
-            user.setEmail("mentoruser" + i + "@dh.com");
-            user.setFirstName("MentorName" + i);
-            user.setLastName("MentorSurname" + i);
-            user.setPassword("qwertyqwerty" + i);
-            user.setRole(User.Role.MENTOR);
-            expectedMentors.add(user);
-        }
-        for (int i = 0; i < 10; i++) {
-            User user = new User();
-            user.setEmail("traineeUser" + i + "@dh.com");
-            user.setFirstName("TraineeName" + i);
-            user.setLastName("TraineeSurname" + i);
-            user.setPassword("qwerty^qwerty" + i);
-            user.setRole(User.Role.TRAINEE);
-            expectedTrainees.add(user);
+        for (int i = 0; i < 2; i++) {
+            User mentor = new User();
+            mentor.setEmail("mentoruser" + i + "@dh.com");
+            mentor.setFirstName("MentorName" + i);
+            mentor.setLastName("MentorSurname" + i);
+            mentor.setPassword("qwertyqwerty" + i);
+            mentor.setRole(User.Role.MENTOR);
+            expectedMentors.add(mentor);
+
+            User trainee = new User();
+            trainee.setEmail("traineeUser" + i + "@dh.com");
+            trainee.setFirstName("TraineeName" + i);
+            trainee.setLastName("TraineeSurname" + i);
+            trainee.setPassword("qwerty^qwerty" + i);
+            trainee.setRole(User.Role.TRAINEE);
+            expectedTrainees.add(trainee);
         }
 
         Assertions.assertEquals(expectedMentors, actualMentors, "checkGetAllMentors()");
         Assertions.assertEquals(expectedTrainees, actualTrainees, "checkGetAllTrainees()");
+    }
+
+    @Test
+    @Order(2)
+    public void checkUpdateUsers() {
+        User mentor1 = userService.getUserById(1L);
+        User mentor2 = userService.getUserById(3L);
+        User trainee1 = userService.getUserById(2L);
+        List<User> actual = new ArrayList<>();
+        actual.add(mentor1);
+        actual.add(mentor2);
+        actual.add(trainee1);
+        mentor1.setFirstName("ChangedMentor1");
+        mentor2.setFirstName("ChangedMentor2");
+        trainee1.setFirstName("ChangedTrainee1");
+        userService.createOrUpdateUser(mentor1);
+        userService.createOrUpdateUser(mentor2);
+        userService.createOrUpdateUser(trainee1);
+
+        List<User> expected = new ArrayList<>();
+
+        User user1 = new User();
+        user1.setEmail("mentoruser0@dh.com");
+        user1.setFirstName("ChangedMentor1");
+        user1.setLastName("MentorSurname0");
+        user1.setPassword("qwertyqwerty0");
+        user1.setRole(User.Role.MENTOR);
+        expected.add(user1);
+
+        User user2 = new User();
+        user2.setEmail("mentoruser1@dh.com");
+        user2.setFirstName("ChangedMentor2");
+        user2.setLastName("MentorSurname1");
+        user2.setPassword("qwertyqwerty1");
+        user2.setRole(User.Role.MENTOR);
+        expected.add(user2);
+
+        User user3 = new User();
+        user3.setEmail("traineeUser0@dh.com");
+        user3.setFirstName("ChangedTrainee1");
+        user3.setLastName("TraineeSurname0");
+        user3.setPassword("qwerty^qwerty0");
+        user3.setRole(User.Role.TRAINEE);
+        expected.add(user3);
+
+        Assertions.assertEquals(expected, actual, "checkUpdateUsers()");
     }
 }
