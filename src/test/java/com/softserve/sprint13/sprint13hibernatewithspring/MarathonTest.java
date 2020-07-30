ddea6c466910f3d453a0147b41bf269e342aeba3
Yurii Zaluskyi <yurazaluskyi@gmail.com>
2020-07-30 21:30:33 +0300

    add method getAllMarathonTest to class MarathonTest

---
+++ src/test/java/com/softserve/sprint13/sprint13hibernatewithspring/MarathonTest.java
@@ -0,0 +1,35 @@
+package com.softserve.sprint13.sprint13hibernatewithspring;
+
+import com.softserve.sprint13.entity.Marathon;
+import com.softserve.sprint13.service.MarathonService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.client.match.MockRestRequestMatchers;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+public class MarathonTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  private MarathonService marathonService;
+
+  public void getAllMarathonTest() throws Exception {
+    List<Marathon> expected = marathonService.getAll();
+
+    mockMvc.perform(MockMvcRequestBuilders.get("/marathons"))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.model().attributeExists("marathons"))
+        .andExpect(MockMvcResultMatchers.model().attribute("marathons", expected));
+  }
+
+
+}
